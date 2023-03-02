26ad595742a61c4c993ec1ec2785bf2eb90a8101
CRISTHIAN ANDRES CABEZAS MATEUS <candrescabezas@ucundinamarca.edu.co>
2023-03-02 18:42:41 -0500

    Se agrega controlador de Login

---
+++ LineaIII/Controllers/Login/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LineaIII.Controllers.Login
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet(Name = "Login")]
+        public IActionResult login()
+        {
+            return Ok("Ya estoy funcionando");
+        }
+    }
+}
