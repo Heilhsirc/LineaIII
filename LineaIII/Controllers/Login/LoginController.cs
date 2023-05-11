019618d5b233fab48bb9e6dfd75e9d3518930c8f
heilhsirc <moshimio@hotmail.com>
2023-05-11 14:38:55 -0500

    Login

--- LineaIII/Controllers/Login/LoginController.cs
+++ LineaIII/Controllers/Login/LoginController.cs
@@ -1,19 +1,34 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using LineaIII.Data;
+using LineaIII.Modelo;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LineaIII.Controllers.Login
 {
     [ApiController]
     [Route("api/[controller]")]
-    [Authorize]
     public class LoginController : ControllerBase
     {
-        
-        [HttpGet]
-        [Route("Login")]
-        public IActionResult login()
+        private readonly DBContext _context;
+
+        public LoginController(DBContext context)
         {
-            return Ok("Ya estoy funcionando");
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult login([FromBody] Usuario user)
+        {
+            Usuario usuario = _context.Usuarios.Where(u => u.Password.Equals(user.Password) && u.Username.Equals(user.Username)).FirstOrDefault();
+            if (usuario == null)
+            {
+                return BadRequest("Usuario no existe");
+            }
+            else
+            {
+                return Ok(true);
+            }
         }
     }
 }
